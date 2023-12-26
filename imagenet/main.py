12b02bbfa9eda0031e28a242855428509941c97d
Hee Suk Yoon <tomyoon2@gmail.com>
2023-12-26 12:05:31 +0000

    .

--- imagenet/main.py
+++ imagenet/main.py
@@ -413,57 +413,6 @@ class Summary(Enum):
 	SUM = 2
 	COUNT = 3
 
-class AverageMeter(object):
-	"""Computes and stores the average and current value"""
-	def __init__(self, name, fmt=':f', summary_type=Summary.AVERAGE):
-		self.name = name
-		self.fmt = fmt
-		self.summary_type = summary_type
-		self.reset()
-
-	def reset(self):
-		self.val = 0
-		self.avg = 0
-		self.sum = 0
-		self.count = 0
-
-	def update(self, val, n=1):
-		self.val = val
-		self.sum += val * n
-		self.count += n
-		self.avg = self.sum / self.count
-
-	def all_reduce(self):
-		if torch.cuda.is_available():
-			device = torch.device("cuda")
-		elif torch.backends.mps.is_available():
-			device = torch.device("mps")
-		else:
-			device = torch.device("cpu")
-		total = torch.tensor([self.sum, self.count], dtype=torch.float32, device=device)
-		dist.all_reduce(total, dist.ReduceOp.SUM, async_op=False)
-		self.sum, self.count = total.tolist()
-		self.avg = self.sum / self.count
-
-	def __str__(self):
-		fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
-		return fmtstr.format(**self.__dict__)
-	
-	def summary(self):
-		fmtstr = ''
-		if self.summary_type is Summary.NONE:
-			fmtstr = ''
-		elif self.summary_type is Summary.AVERAGE:
-			fmtstr = '{name} {avg:.3f}'
-		elif self.summary_type is Summary.SUM:
-			fmtstr = '{name} {sum:.3f}'
-		elif self.summary_type is Summary.COUNT:
-			fmtstr = '{name} {count:.3f}'
-		else:
-			raise ValueError('invalid summary type %r' % self.summary_type)
-		
-		return fmtstr.format(**self.__dict__)
-
 
 class ProgressMeter(object):
 	def __init__(self, num_batches, meters, prefix=""):
