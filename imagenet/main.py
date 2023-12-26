c47c59983b4f1cafbff28e0de76025b6a6e5fe69
Hee Suk Yoon <tomyoon2@gmail.com>
2023-12-26 12:08:31 +0000

    .

--- imagenet/main.py
+++ imagenet/main.py
@@ -406,44 +406,6 @@ def main_worker(gpu, ngpus_per_node, args):
 
 			print('end logging')
 		
-class ProgressMeter(object):
-	def __init__(self, num_batches, meters, prefix=""):
-		self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
-		self.meters = meters
-		self.prefix = prefix
-
-	def display(self, batch):
-		entries = [self.prefix + self.batch_fmtstr.format(batch)]
-		entries += [str(meter) for meter in self.meters]
-		print('\t'.join(entries))
-		
-	def display_summary(self):
-		entries = [" *"]
-		entries += [meter.summary() for meter in self.meters]
-		print(' '.join(entries))
-
-	def _get_batch_fmtstr(self, num_batches):
-		num_digits = len(str(num_batches // 1))
-		fmt = '{:' + str(num_digits) + 'd}'
-		return '[' + fmt + '/' + fmt.format(num_batches) + ']'
-
-def accuracy(output, target, topk=(1,)):
-	"""Computes the accuracy over the k top predictions for the specified values of k"""
-	with torch.no_grad():
-		maxk = max(topk)
-		batch_size = target.size(0)
-
-		_, pred = output.topk(maxk, 1, True, True)
-		pred = pred.t()
-		correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-		res = []
-		for k in topk:
-			correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
-			res.append(correct_k.mul_(100.0 / batch_size))
-		return res
-
-
 def tempscale_train(args, model, device, logits_list, labels_list):
 
 	#load saved model
